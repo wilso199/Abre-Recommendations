48796cd6c8dc658c4740af7b6eceabffa6e0acaa
Christopher Rose <crose@hcsdoh.org>
2017-09-26 15:35:01 -0400

    Version Update

--- config.php
+++ config.php
@@ -28,7 +28,7 @@
 	$pageorder=4;
 	$pagetitle="Recommendations";
 	$description="Teacher recommendation and course placement calculation system.";
-	$version="0.0.6";
+	$version="0.0.7";
 	$repo="abreio/Abre-Recommendations";
 	$pageicon="check_circle";
 	$pagepath="recommendations";
